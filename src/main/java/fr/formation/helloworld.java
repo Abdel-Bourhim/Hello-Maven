d8002317f74624f4deea3054c712afed8f397e4e
Abdelhamid Bourhim <113910939+Abdel-Bourhim@users.noreply.github.com>
2022-10-10 14:40:16 +0200

    Update helloworld.java

--- src/main/java/fr/formation/helloworld.java
+++ src/main/java/fr/formation/helloworld.java
@@ -3,10 +3,7 @@ package fr.formation;
 public class helloworld {
 
 	public static void main(String[] args) {
-		
-		String message = "Hello maven world";
-		afficher A = new afficher() ;
-		A.display(message);
+		System.out.println('Hello maven world');
 
 }
 
